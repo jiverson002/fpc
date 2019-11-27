5c8cc23b7cae7ca24364ea0555e3ba822b3fbbcb
Jeremy Iverson <jiverson002@csbsju.edu>
2019-11-27 14:44:59 -0600

    fpc: Updated with new version of fpc from Burtscher website

--- fpc.h
+++ fpc.h
@@ -3,36 +3,13 @@
 
 #include <stdlib.h>
 
-struct fpc_context {
-  unsigned long hash;
-  unsigned long dhash;
-  unsigned long long lastval;
-  unsigned long long pred1;
-  unsigned long long pred2;
-  unsigned long long * wrkmem;
-};
-
 #if defined (__cplusplus)
 extern "C" {
 #endif
 
-extern size_t
-fpc_compress(
-  unsigned long const predsizem1,
-  void const * const in,
-  void * const out,
-  size_t const size,
-  struct fpc_context * const ctx
-);
-
-extern size_t
-fpc_decompress(
-  unsigned long const predsizem1,
-  void const * const in,
-  void * const out,
-  size_t const size,
-  struct fpc_context * const ctx
-);
+void fpc_minit(void const*, void*, size_t);
+size_t fpc_compress(long, void const*, void*, size_t);
+size_t fpc_decompress(void const*, void*, size_t);
 
 #if defined (__cplusplus)
 }
